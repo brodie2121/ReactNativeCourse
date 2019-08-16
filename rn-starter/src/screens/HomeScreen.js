78ee6e04e7a8b5ae24e570fe7e02676d90a6c5d9
Brodie Brock <brodiebrock@gmail.com>
2019-08-15 21:16:57 -0500

    created random color generator

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,36 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello There!</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to components demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to list demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to Images"
+      />
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to counter demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Color")}
+        title="Go to color demo"
+      />
+      <Button
+        onPress={() => navigation.navigate("Square")}
+        title="Go to square demo"
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
